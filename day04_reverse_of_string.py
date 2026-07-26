1d6fc9272b5d5da193c188ce16a042800d1fc41c
Anji Embadi <embadianji1@gmail.com>
2026-07-26 11:44:38 +0530

    Delete day04_reverse_of_string.py

--- day04_reverse_of_string.py
+++
@@ -1,8 +0,0 @@
-#write a function to reverse a string
-def reverse_string(n):
-          rev=""
-          for ch in n:
-                  rev=ch+rev
-          return rev
-s=input()
-print(reverse_string(s))
